2d1787d97882bee4dabc737767fd9e9c62bd2c21
Arya Anish <anishkumarsarmi@gmail.com>
2021-06-27 14:27:42 +0530

    changed files

--- app.js
+++ app.js
@@ -62,6 +62,14 @@ passport.deserializeUser(function (id, done) {
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.post('/isAuth', (req, res) => {
   if (req.isAuthenticated()) {
     res.send('yes');
